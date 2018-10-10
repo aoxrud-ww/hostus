98a90543f0d891ade3557f7442070d7a8914cd8d
Alex Oxrud <alex.oxrud@weightwatchers.com>
2018-10-10 13:15:29 -0600

    Added buttonbar

---
+++ src/components/Buttonbar/Buttonbar.js
@@ -0,0 +1,58 @@
+import React, { PureComponent } from 'react';
+import styles from './Buttonbar.module.scss';
+import Button from '../Button/Button.js';
+
+class Buttonbar extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.click = this.click.bind(this);
+    this.state = {
+      selected: Number(this.props.value),
+      initialValue: this.props.value
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    const valuePropChanged = props.value !== state.initialValue;
+    const valuePropDifferentThanState = props.value !== state.value;
+    if(valuePropChanged && valuePropDifferentThanState) {
+      state.value = props.value;
+      state.initialValue = props.value;
+    }
+    return state;
+  }
+
+  click(button, index) {
+    return e => {
+
+      if(this.props.updateSelected) {
+        this.setState({
+          selected: index
+        });
+      }
+      button.props.onClick(e, button.props);
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.buttonbar}>
+        {this.props.children.map((button, index) => {
+          const theme = this.state.selected === index ? 'buttonbar-selected' : 'buttonbar';
+          return (
+            <Button key={index} theme={theme} onClick={this.click(button, index)}>
+            {button.props.children}
+            </Button>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+Buttonbar.defaultProps = {
+  updateSelected: true
+}
+
+
+export default Buttonbar;
