ad3988617979da31b7cef6695e9200379d1ce706
Alex Oxrud <alex.oxrud@weightwatchers.com>
2018-10-09 21:12:51 -0600

    Fixed rendering issue with increment input and added proptypes It now updates a local ref to prevent re-rendering entire component

--- src/components/IncrementInput/IncrementInput.js
+++ src/components/IncrementInput/IncrementInput.js
@@ -4,33 +4,51 @@ import Button from "../Button/Button.js";
 import ReactSVG from 'react-svg';
 import iconPlus from '../../assets/add.svg';
 import iconMinus from '../../assets/subtract.svg';
+import PropTypes from 'prop-types';
 
 class IncrementInput extends Component {
-
   constructor(props) {
     super(props);
+    this.state = {
+      value: this.props.value,
+      initialValue: this.props.value
+    };
     this.didChange = this.didChange.bind(this);
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
+    this.inputRef = React.createRef();
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    const valuePropChanged = props.value !== state.initialValue;
+    const valuePropDifferentThanState = props.value !== state.value;
+    if(valuePropChanged && valuePropDifferentThanState) {
+      state.value = props.value;
+      state.initialValue = props.value;
+    }
+    return state;
   }
 
   didChange(e) {
-    this.props.onChange(e.target.value);
+    this.setState({value: e.target.value}, () => {
+      this.broadcastValue(this.state.value);
+    });
+  }
+
+  broadcastValue(value) {
+    this.inputRef.current.value = value;
+    this.props.onChange(value);
   }
 
   increment() {
-    if(this.props.value) {
-      const value = Number(this.props.value) + 1;
-      this.props.onChange(value);
-    }
+    const value = Number(this.inputRef.current.value) + this.props.step;
+    this.broadcastValue(value);
   }
 
-
   decrement() {
-    const value = Number(this.props.value) - 1;
-
-    if(value > 0) {
-      this.props.onChange(value);
+    const value = Number(this.inputRef.current.value) - this.props.step;
+    if(this.props.allowZero || value > 0) {
+      this.broadcastValue(value);
     }
   }
 
@@ -41,7 +59,7 @@ class IncrementInput extends Component {
           <ReactSVG svgClassName={styles.incrementIcons} src={iconMinus} />
         </Button>
         <div className={styles.inputContainer}>
-          <input className={styles.input} value={this.props.value} onChange={this.didChange} />
+          <input className={styles.input} value={this.state.value} onChange={this.didChange} ref={this.inputRef} />
         </div>
         <Button theme="icon" onClick={this.increment}>
           <ReactSVG svgClassName={styles.incrementIcons} src={iconPlus} />
@@ -51,9 +69,17 @@ class IncrementInput extends Component {
   }
 }
 
+IncrementInput.propTypes = {
+  value: PropTypes.number,
+  step: PropTypes.number,
+  allowZero: PropTypes.bool,
+  onChange: PropTypes.func.isRequired
+};
 
 IncrementInput.defaultProps = {
   value: 1,
+  step: 1,
+  allowZero: false,
   onChange: () => {}
 };
 
