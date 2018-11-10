94d55cb34b91ed0b8c04d34081d86f864eea52af
Alex Oxrud <alex.oxrud@gmail.com>
2018-11-10 15:09:25 -0700

    Updated search error components

--- src/components/SearchError/SearchError.js
+++ src/components/SearchError/SearchError.js
@@ -2,9 +2,19 @@ import React, { PureComponent } from 'react';
 import styles from './SearchError.module.scss';
 import searchLoupe from '../../assets/search.svg';
 import ReactSVG from 'react-svg';
+import PropTypes from 'prop-types';
 
-class SearchError extends PureComponent {
+class EmptyResults extends PureComponent {
+  render() {
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.title}>There are no parties on the waitlist.</h1>
+      </div>
+    );
+  }
+}
 
+class NotFound extends PureComponent {
   render() {
     return (
       <div className={styles.container}>
@@ -14,5 +24,32 @@ class SearchError extends PureComponent {
     );
   }
 }
+
+NotFound.defaultProps = {
+  query: ''
+};
+
+NotFound.propTypes = {
+  query: PropTypes.string
+};
+
+
+class SearchError extends PureComponent {
+  render() {
+    if(this.props.query) {
+      return <NotFound query={this.props.query} />;
+    } else {
+      return <EmptyResults />;
+    }
+  }
+}
+
+SearchError.defaultProps = {
+  query: ''
+};
+
+SearchError.propTypes = {
+  query: PropTypes.string
+};
 
 export default SearchError;
